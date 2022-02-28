e8df589f2f78482d9b5e5f66a7c668f968312426
walid rahim <ar_olid@outlook.com>
2022-02-28 02:29:57 -0700

    final commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import "./Header.scss";
-import LogoUpload from "../../Assets/Icons/upload.svg";
 import Logo from "../../Assets/Logo/BrainFlix-logo.svg";
 import Pfp from "../../Assets/Images/Mohan-muruge.jpg";
 
